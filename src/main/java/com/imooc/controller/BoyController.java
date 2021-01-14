bc0f2fe3a0bd5a0ab262fd33a779bccce399c55c
yigx <yigx@akulaku.com>
2021-01-14 10:47:32 +0800

    add boy controller

---
+++ src/main/java/com/imooc/controller/BoyController.java
@@ -0,0 +1,31 @@
+package com.imooc.controller;
+
+import com.imooc.properties.GirlProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Administrator on 2017/10/15 0015.
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+//    @Value("${cupSize}")
+//    private String cupSize;
+//
+//    @Value("${age}")
+//    private Integer age;
+//    //这里的简单类型也可以
+//    //private int age;
+//
+//    @Value("${content}")
+//    private String content;
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @GetMapping(value = "/say")
+    public String sayHello(@RequestParam(value="id",required = false,defaultValue = "0") Integer id){
+        return "id: "+id;
+    } 
+}
